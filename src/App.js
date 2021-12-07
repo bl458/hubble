35bc7a46b815a51ef042d0313b25065eb3e313e2
Byungchan Lim <bl458@cornell.edu>
2021-12-07 17:43:27 -0500

    FEAT create content cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import Card from "./components/Card";
 
 import Header from "./components/Header";
 
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
+
+import CARD_CONTENT from "./constants/CardContent";
 
 const theme = {
   colors: {
@@ -20,7 +23,11 @@ function App() {
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Header />
-      <Container></Container>
+      <Container>
+        {CARD_CONTENT.map((item, idx) => (
+          <Card key={idx} item={item} />
+        ))}
+      </Container>
     </ThemeProvider>
   );
 }
