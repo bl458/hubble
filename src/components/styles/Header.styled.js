a7ca13eb6f84c3e7e57a1198f24ad43a18901220
Byungchan Lim <bl458@cornell.edu>
2021-12-07 16:30:55 -0500

    FIX container, header style

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -3,12 +3,4 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.colors.header};
   padding: 40px 0;
-
-  h1 {
-    color: red;
-  }
-
-  &:hover {
-    background-color: yellow;
-  }
 `;
